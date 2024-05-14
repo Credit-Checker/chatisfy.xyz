efea137979faa352a5c4b7cb6f5e8584a02fad0b
Micheal Daralola <michaeldaralola123@gmail.com>
2024-05-14 09:30:55 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -6,3 +6,36 @@
   </div>
 </template>
 
+<script>
+export default {
+  mounted() {
+    this.loadFacebookSDK();
+  },
+  methods: {
+    loadFacebookSDK() {
+      // Check if the Facebook SDK script is already loaded
+      if (!document.getElementById('facebook-jssdk')) {
+        // Load the Facebook SDK script
+        const js = document.createElement('script');
+        js.id = 'facebook-jssdk';
+        js.src = 'https://connect.facebook.net/en_US/sdk.js';
+        document.getElementsByTagName('head')[0].appendChild(js);
+        
+        // Initialize the Facebook SDK
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId: '405263659170044',
+            xfbml: true,
+            version: 'v19.0'
+          });
+          FB.AppEvents.logPageView();
+        };
+      } else {
+        // If the Facebook SDK script is already loaded, initialize it immediately
+        window.fbAsyncInit();
+      }
+    }
+  }
+}
+</script>
+
